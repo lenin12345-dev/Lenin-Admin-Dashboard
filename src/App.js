4e05b0045a1f3ee3fb94876d2a077ff9850fa78e
leninm <leninm@lecturenotes.in>
2023-07-12 07:50:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material/styles";
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import CssBaseline from '@mui/material/CssBaseline';
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
+import Geography from "scenes/geography";
+import Overview from "scenes/overview";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
+import Admin from "scenes/admin";
+import Performance from "scenes/performance";
+
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <BrowserRouter>
+          <Routes>
+            {/* Layout is uniform in all pages like navbar or footer */}
+            <Route element={<Layout />}>      
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+                <Route path="/transactions" element={<Transactions />} />
+               <Route path="/geography" element={<Geography />} /> 
+               <Route path="/overview" element={<Overview />} />
+               <Route path="/daily" element={<Daily />} />
+               <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
+               <Route path="/admin" element={<Admin />} />
+              <Route path="/performance" element={<Performance />} />     
+            </Route>
+          </Routes>
+          </BrowserRouter>
+          </ThemeProvider>
     </div>
   );
 }
